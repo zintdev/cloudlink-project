4aa32f5d19e7f58bc099a32169c1c667295d2683
zintdev <144522038+zintdev@users.noreply.github.com>
2025-11-11 22:41:35 +0700

    Initialization

---
+++ cloudlink-backend/src/main/java/com/zintdev/cloudlink_backend/CloudlinkBackendApplication.java
@@ -0,0 +1,13 @@
+package com.zintdev.cloudlink_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudlinkBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudlinkBackendApplication.class, args);
+	}
+
+}
